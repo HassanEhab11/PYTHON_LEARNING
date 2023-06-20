90df4b36b36e6c21c74b36648658daefd256c480
@hassanehab11 <122078183+HassanEhab11@users.noreply.github.com>
2023-06-21 00:35:40 +0300

    Update WeightConvertedProgram.py

--- WeightConvertedProgram.py
+++ WeightConvertedProgram.py
@@ -1,1 +1,13 @@
 
+
+weight = int(input("Weight : "))
+unit = input("(L)bs or (K)g: ")
+if unit.upper() == 'L' :
+    converted = weight * .45
+    print(f'You are   {converted} kilos')
+elif unit.upper() == 'K':
+    converted = weight / .45
+    print(f'You are   {converted} pounds')
+else:
+    print('choose correct')
+print("With best wishes")
